7b3e6259599bc73cc96f2997afd551f794dd28d4
Muthi'ah Sahid <muthi.ah756@smk.belajar.id>
2025-10-03 11:01:56 +0800

    Penambahan AuthController & perubahan route

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        Auth::login($user);
+
+        return redirect()->route('login.form')->with('success', 'Berhasil Register, silakan login.');
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password'=> 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $request->session()->regenerate();
+            return redirect()->route('dashboard')->with('success', 'Berhasil Login');
+        }
+
+        return back()->with('error', 'Email atau Password salah');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.form')->with('success', 'Berhasil Logout');
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+// Welcome
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+// Register
+Route::get('/register', function () {
+    return view('register');
+})->name('register.form');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+// Login
+Route::get('/login', function () {
+    return view('login');
+})->name('login.form');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// Dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
